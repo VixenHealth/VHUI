a09a6dfb40d19b20554f96a07b2f4b223cebf72a
darklorian <lolpokens@mail.ru>
2024-07-12 23:26:16 +0300

    fix scss no load

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,7 +9,14 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
-    "@storybook/preset-scss",
+    {
+      name: "@storybook/preset-scss",
+      options: {
+        cssLoaderOptions: {
+          modules: false
+        }
+      }
+    }
   ],
   framework: {
     name: "@storybook/react-webpack5",
