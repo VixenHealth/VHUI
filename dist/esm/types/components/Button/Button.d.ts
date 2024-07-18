6bbd2a3ff18a3794e1a28d292fa12309b1cdfdd1
Nastya <elgina.anasta@yandex.ru>
2024-07-18 22:39:08 +0400

    Changed exports

---
+++ dist/esm/types/components/Button/Button.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+export interface ButtonProps {
+    labell: string;
+}
+declare const Button: (props: ButtonProps) => React.JSX.Element;
+export default Button;
---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface ButtonProps {
+    labell: string;
+}
+declare const Button: (props: ButtonProps) => React.JSX.Element;
+
+export { Button };
