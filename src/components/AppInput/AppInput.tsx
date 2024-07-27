77671abffee47739c029462493b46d84dcd93348
Nastya <elgina.anasta@yandex.ru>
2024-07-28 00:38:12 +0400

    Changed AppInput

--- src/components/AppInput/AppInput.tsx
+++ src/components/AppInput/AppInput.tsx
@@ -19,6 +19,8 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const AppInput = forwardRef<HTMLInputElement, Props>(({ inputSize, ...props}, ref) => {
+  console.log(props)
+  
   return (
     <input
       ref={ref}
