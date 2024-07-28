bb445d41b97724abeac43d5131aa6abd483df08c
darklorian <lolpokens@mail.ru>
2024-07-28 23:44:26 +0400

    remove dist

--- dist/esm/types/components/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export interface ButtonProps {
-    labell: string;
-}
-declare const Button: (props: ButtonProps) => React.JSX.Element;
-export default Button;
--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-interface ButtonProps {
-    labell: string;
-}
-declare const Button: (props: ButtonProps) => React.JSX.Element;
-
-export { Button };
