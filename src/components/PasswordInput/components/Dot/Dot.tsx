d60bb323f2e8bed52e85462f5d85ea34545c4095
darklorian <lolpokens@mail.ru>
2024-07-30 03:24:09 +0400

    try to fix dots

--- src/components/PasswordInput/components/Dot/Dot.tsx
+++ src/components/PasswordInput/components/Dot/Dot.tsx
@@ -4,50 +4,50 @@ import classNames from "classnames/bind";
 import styles from "./style.module.scss";
 
 const cx = classNames.bind(styles);
+export const DotAnimation: React.FC<{ dotCount: number }> = ({ dotCount }) => {
+	const dotsContainerRef = useRef<HTMLDivElement | null>(null);
 
-export const Dot = () => {
-	const ref = useRef(null);
-	const minLifetime = 2000; // Минимальное время жизни точки в миллисекундах
-	const maxLifetime = 5000; // Максимальное время жизни точки в миллисекундах
-	
-	const getStyles = () => {
-		const x: string = `${Math.random() * 100}%`;
-		const y: string = `${Math.random() * 100}%`;
-		
-		return {
-			top: x,
-			left: y,
-		}
-	}
-	
-	function resetDot(dot: any) {
-		const x = Math.random() * 200 - 100;
-		const y = Math.random() * 100 - 50;
-		dot.style.setProperty('--x', `${x}px`);
-		dot.style.setProperty('--y', `${y}px`);
-		
-		const lifetime = (Math.random() * (maxLifetime - minLifetime) + minLifetime);
-		const small_lifetime = `${lifetime / 1000}s`
-		dot.style.animationDuration = small_lifetime;
-		
-		dot.style.animation = 'none';
-		dot.offsetHeight;
-		dot.style.animation = `move ${small_lifetime} linear, fadeOut ${small_lifetime} ease-out forwards`;
-		
-		setTimeout(() => {
-			resetDot(dot);
-		}, 0);
-	}
-	
 	useEffect(() => {
-		resetDot(ref.current);
-	}, [ref.current]);
-	
-	return (
-		<div
-			ref={ref}
-			className={cx("dot")}
-			style={getStyles()}
-		></div>
-	);
-}
+		const dotsContainer = dotsContainerRef.current;
+		if (!dotsContainer) return;
+
+		const dots: HTMLDivElement[] = [];
+		const minLifetime = 2000;
+		const maxLifetime = 5000;
+
+		const resetDot = (dot: HTMLDivElement) => {
+			const x = Math.random() * 200 - 100;
+			const y = Math.random() * 100 - 50;
+			dot.style.setProperty('--x', `${x}px`);
+			dot.style.setProperty('--y', `${y}px`);
+
+			const lifetime = Math.random() * (maxLifetime - minLifetime) + minLifetime;
+			const smallLifetime = `${lifetime / 1000}s`;
+			dot.style.animationDuration = smallLifetime;
+
+			dot.style.animation = 'none';
+			dot.offsetHeight;
+			dot.style.animation = `move ${smallLifetime} linear, fadeOut ${smallLifetime} ease-out forwards`;
+
+			setTimeout(() => {
+				resetDot(dot);
+			}, lifetime);
+			console.log("re-render")
+		};
+
+		for (let i = 0; i < dotCount; i++) {
+			const dot = document.createElement('div');
+			dot.classList.add(cx('dot'));
+			dot.style.top = `${Math.random() * 100}%`;
+			dot.style.left = `${Math.random() * 100}%`;
+			dotsContainer.appendChild(dot);
+			dots.push(dot);
+			resetDot(dot);
+		}
+		return () => {
+			dots.forEach(dot => dotsContainer.removeChild(dot));
+		};
+	}, [dotCount]);
+
+	return <div className={cx("dots")} ref={dotsContainerRef}></div>;
+};
