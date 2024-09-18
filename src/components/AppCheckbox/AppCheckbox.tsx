0dc76724db15bd1cfae34525adaac105547041e0
Nastya <elgina.anasta@yandex.ru>
2024-09-18 18:47:50 +0400

    Changed AppCheckbox component

--- src/components/AppCheckbox/AppCheckbox.tsx
+++ src/components/AppCheckbox/AppCheckbox.tsx
@@ -8,28 +8,25 @@ const cx = classNames.bind(styles);
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
 	label: string;
-	id: string;
 	checked?: boolean;
 	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const AppCheckbox: FC<Props> = forwardRef<HTMLInputElement, Props>(({
 	                                                                           label,
-	                                                                           id,
 	                                                                           checked,
 	                                                                           onChange,
 	                                                                           style,
 	                                                                           ...props
                                                                            }, ref) => {
 	return (
-		<label style={{...style}} className={cx("app-checkbox")} htmlFor={id}>
+		<label style={{...style}} className={cx("app-checkbox")}>
 			<input
 				ref={ref}
 				className={cx("app-checkbox__input")}
 				checked={checked}
 				onChange={onChange}
 				type="checkbox"
-				id={id}
 				{...props}
 			/>
 			<span className={cx("app-checkbox__box")}></span>
