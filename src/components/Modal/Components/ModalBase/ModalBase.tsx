24bd04933bb5713e3b6614526e8bf640ab3d609f
Nastya <elgina.anasta@yandex.ru>
2024-10-14 13:11:43 +0400

    Fixed Modal component

--- src/components/Modal/Components/ModalBase/ModalBase.tsx
+++ src/components/Modal/Components/ModalBase/ModalBase.tsx
@@ -4,16 +4,20 @@ import classNames from "classnames/bind";
 import {VariantModalBase} from "../../types/variants";
 
 import styles from "./style.module.scss";
+import CloseIcon from "../../../../assets/modal-close.svg";
 
 const cx = classNames.bind(styles);
 
-export const ModalBase: FC<VariantModalBase> = ({style, children }) => {
+export const ModalBase: FC<VariantModalBase> = ({style, children, onClose }) => {
 	const onContentClick = (e: React.MouseEvent) => {
 		e.stopPropagation();
 	};
 	
 	return (
 		<div style={{...style}} className={cx("content")} onClick={onContentClick}>
+			<div onClick={onClose} className={cx("content__close")}>
+				<img src={CloseIcon} alt=""/>
+			</div>
 			{children}
 		</div>
 	)
--- src/components/Modal/Components/ModalDouble/ModalDouble.tsx
+++ src/components/Modal/Components/ModalDouble/ModalDouble.tsx
@@ -2,18 +2,22 @@ import React, {FC} from "react";
 import classNames from "classnames/bind";
 
 import {VariantModalDouble} from "../../types/variants";
+import CloseIcon from "../../../../assets/modal-close.svg";
 
 import styles from "./style.module.scss";
 
 const cx = classNames.bind(styles);
 
-export const ModalDouble: FC<VariantModalDouble> = ({style, topContentChildren, bottomContentChildren}) => {
+export const ModalDouble: FC<VariantModalDouble> = ({style, topContentChildren, bottomContentChildren, onClose}) => {
 	const onContentClick = (e: React.MouseEvent) => {
 		e.stopPropagation();
 	};
 	
 	return (
 		<div style={{...style}} className={cx("content")} onClick={onContentClick}>
+			<div onClick={onClose} className={cx("content__close")}>
+				<img src={CloseIcon} alt=""/>
+			</div>
 			<div className={cx("content__top")}>{topContentChildren}</div>
 			<div className={cx("content__bottom")}>{bottomContentChildren}</div>
 		</div>
